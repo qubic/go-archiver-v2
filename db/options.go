7cfdae4682735fd261a66f39b3f4b6b7985e15fd
mio <180114449+qubicmio@users.noreply.github.com>
2025-09-20 14:12:56 +0000

    Disable l2/l3 options because it breaks loading last processed tick

--- db/options.go
+++ db/options.go
@@ -9,6 +9,9 @@ const memTableSize = 268435456 // 256MB
 
 func getDefaultPebbleOptions() *pebble.Options {
 
+	// FIXME enabling more than the l1 options breaks storing latest tick
+	// to test delete current db and run. Stop service and run again.
+
 	l1Options := pebble.LevelOptions{
 		BlockRestartInterval: 16,
 		BlockSize:            4096,
@@ -20,30 +23,31 @@ func getDefaultPebbleOptions() *pebble.Options {
 		TargetFileSize:       memTableSize, // 256 MB
 	}
 
-	l2Options := pebble.LevelOptions{
-		BlockRestartInterval: 16,
-		BlockSize:            4096,
-		BlockSizeThreshold:   90,
-		Compression:          pebble.ZstdCompression,
-		FilterPolicy:         nil,
-		FilterType:           pebble.TableFilter,
-		IndexBlockSize:       4096,
-		TargetFileSize:       l1Options.TargetFileSize * 10, // 2.5 GB
-	}
-
-	l3Options := pebble.LevelOptions{
-		BlockRestartInterval: 16,
-		BlockSize:            4096,
-		BlockSizeThreshold:   90,
-		Compression:          pebble.ZstdCompression,
-		FilterPolicy:         nil,
-		FilterType:           pebble.TableFilter,
-		IndexBlockSize:       4096,
-		TargetFileSize:       l2Options.TargetFileSize * 10, // 25 GB
-	}
+	//l2Options := pebble.LevelOptions{
+	//	BlockRestartInterval: 16,
+	//	BlockSize:            4096,
+	//	BlockSizeThreshold:   90,
+	//	Compression:          pebble.ZstdCompression,
+	//	FilterPolicy:         nil,
+	//	FilterType:           pebble.TableFilter,
+	//	IndexBlockSize:       4096,
+	//	TargetFileSize:       l1Options.TargetFileSize * 10, // 2.5 GB
+	//}
+	//
+	//l3Options := pebble.LevelOptions{
+	//	BlockRestartInterval: 16,
+	//	BlockSize:            4096,
+	//	BlockSizeThreshold:   90,
+	//	Compression:          pebble.ZstdCompression,
+	//	FilterPolicy:         nil,
+	//	FilterType:           pebble.TableFilter,
+	//	IndexBlockSize:       4096,
+	//	TargetFileSize:       l2Options.TargetFileSize * 10, // 25 GB
+	//}
 
 	pebbleOptions := pebble.Options{
-		Levels: []pebble.LevelOptions{l1Options, l2Options, l3Options},
+		Levels: []pebble.LevelOptions{l1Options},
+		//Levels: []pebble.LevelOptions{l1Options, l2Options, l3Options}, // FIXME leads to data corruption (last processed tick)
 		MaxConcurrentCompactions: func() int {
 			return runtime.NumCPU()
 		},
