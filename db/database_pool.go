6f3cf462afd8292b37b75bf2eae5230f8abfd56f
LINCKODE <linckode@gmail.com>
2025-09-24 18:38:32 +0300

    Make database_pool.go CreateStore public.

--- db/database_pool.go
+++ db/database_pool.go
@@ -3,12 +3,13 @@ package db
 import (
 	"errors"
 	"fmt"
-	"github.com/cockroachdb/pebble"
 	"log"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/cockroachdb/pebble"
 )
 
 type DatabasePool struct {
@@ -39,7 +40,7 @@ func (dp *DatabasePool) GetOrCreateDbForEpoch(epoch uint16) (*PebbleStore, error
 	store := dp.stores[epoch]
 	if store == nil {
 		log.Printf("Creating database for epoch [%d].", epoch)
-		newStore, err := createStore(dp.storeDir, epoch)
+		newStore, err := CreateStore(dp.storeDir, epoch)
 		if err != nil {
 			return nil, fmt.Errorf("creating data store for epoch [%d]: %w", epoch, err)
 		}
@@ -91,7 +92,7 @@ func loadFromDisk(directory string) (map[uint16]*PebbleStore, error) {
 				return nil, fmt.Errorf("parsing epoch: %w", err)
 			}
 
-			store, err := createStore(directory, uint16(epoch))
+			store, err := CreateStore(directory, uint16(epoch))
 			if err != nil {
 				return nil, fmt.Errorf("creating data store for epoch [%d]: %w", epoch, err)
 			}
@@ -103,7 +104,7 @@ func loadFromDisk(directory string) (map[uint16]*PebbleStore, error) {
 	return databases, nil
 }
 
-func createStore(directory string, epoch uint16) (*PebbleStore, error) {
+func CreateStore(directory string, epoch uint16) (*PebbleStore, error) {
 	db, err := openDatabase(directory, epoch)
 	if err != nil {
 		return nil, err
