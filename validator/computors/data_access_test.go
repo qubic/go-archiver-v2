30d6891e9103b77681c7b18cbe66ab283d499748
LINCKODE <linckode@gmail.com>
2026-01-13 11:53:50 +0200

    Add tests

--- validator/computors/data_access_test.go
+++ validator/computors/data_access_test.go
@@ -2,6 +2,7 @@ package computors
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
 	"os"
 	"testing"
@@ -94,6 +95,129 @@ func TestComputorsDataAccess_WhenLoad_GivenEmptyListStored_ThenReturnEmptyList(t
 
 }
 
+func TestGet_ExistingList_SameSignature_NoFetch(t *testing.T) {
+	dataStore, err := createTestDataStore(t)
+	defer closeDb(dataStore)
+	require.NoError(t, err)
+
+	// Create a signature with known first 8 bytes
+	signature := [64]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}
+	signaturePackage := binary.LittleEndian.Uint64(signature[:8])
+
+	computorsList := []*Computors{{
+		Epoch:     123,
+		PubKeys:   [NumberOfComputors][32]byte{{1}, {2}, {3}},
+		Signature: signature,
+	}}
+
+	client := &TestClient{
+		computors: types.Computors{
+			Epoch:     123,
+			PubKeys:   [NumberOfComputors][32]byte{{1}, {2}, {3}},
+			Signature: [64]byte{0xFF}, // Different signature - should NOT be fetched
+		},
+	}
+
+	err = Save(context.Background(), dataStore, 42, computorsList)
+	require.NoError(t, err)
+
+	// Pass matching signature package - should NOT fetch from node
+	loaded, err := Get(context.Background(), dataStore, client, 100, 0, 42, signaturePackage)
+	require.NoError(t, err)
+	require.False(t, client.getComputorsCalled, "GetComputors should NOT be called when signature matches")
+	require.Len(t, loaded, 1)
+}
+
+func TestGet_ExistingList_NewSignature_AppendsNew(t *testing.T) {
+	dataStore, err := createTestDataStore(t)
+	defer closeDb(dataStore)
+	require.NoError(t, err)
+
+	// Stored computors with one signature
+	storedSignature := [64]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}
+
+	computorsList := []*Computors{{
+		Epoch:      123,
+		TickNumber: 50,
+		PubKeys:    [NumberOfComputors][32]byte{{1}, {2}, {3}},
+		Signature:  storedSignature,
+	}}
+
+	// New computors from node with different signature
+	newSignature := [64]byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18}
+	newSignaturePackage := binary.LittleEndian.Uint64(newSignature[:8])
+
+	client := &TestClient{
+		computors: types.Computors{
+			Epoch:     123,
+			PubKeys:   [NumberOfComputors][32]byte{{4}, {5}, {6}},
+			Signature: newSignature,
+		},
+	}
+
+	err = Save(context.Background(), dataStore, 42, computorsList)
+	require.NoError(t, err)
+
+	// Pass different signature package - should fetch from node and append
+	loaded, err := Get(context.Background(), dataStore, client, 100, 0, 42, newSignaturePackage)
+	require.NoError(t, err)
+	require.True(t, client.getComputorsCalled, "GetComputors should be called when signature differs")
+	require.Len(t, loaded, 2)
+	require.Equal(t, uint32(100), loaded[1].TickNumber, "New computors should have the provided tick number")
+	require.Equal(t, newSignature, loaded[1].Signature)
+}
+
+func TestGet_EmptyStore_SignatureMismatch_Errors(t *testing.T) {
+	dataStore, err := createTestDataStore(t)
+	defer closeDb(dataStore)
+	require.NoError(t, err)
+
+	// Node returns computors with this signature
+	nodeSignature := [64]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}
+
+	client := &TestClient{
+		computors: types.Computors{
+			Epoch:     123,
+			PubKeys:   [NumberOfComputors][32]byte{{1}, {2}, {3}},
+			Signature: nodeSignature,
+		},
+	}
+
+	// Pass a different signature package than what node returns
+	differentSignaturePackage := uint64(0xDEADBEEF)
+
+	// No stored computors - will fetch from node, but signature won't match
+	_, err = Get(context.Background(), dataStore, client, 100, 50, 42, differentSignaturePackage)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "initial computor list signature does not match")
+	require.True(t, client.getComputorsCalled, "GetComputors should be called for initial fetch")
+}
+
+func TestGet_EmptyStore_SignatureMatch_Succeeds(t *testing.T) {
+	dataStore, err := createTestDataStore(t)
+	defer closeDb(dataStore)
+	require.NoError(t, err)
+
+	// Node returns computors with this signature
+	nodeSignature := [64]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}
+	signaturePackage := binary.LittleEndian.Uint64(nodeSignature[:8])
+
+	client := &TestClient{
+		computors: types.Computors{
+			Epoch:     123,
+			PubKeys:   [NumberOfComputors][32]byte{{1}, {2}, {3}},
+			Signature: nodeSignature,
+		},
+	}
+
+	// No stored computors - will fetch from node, signature matches
+	loaded, err := Get(context.Background(), dataStore, client, 100, 50, 42, signaturePackage)
+	require.NoError(t, err)
+	require.True(t, client.getComputorsCalled)
+	require.Len(t, loaded, 1)
+	require.Equal(t, uint32(50), loaded[0].TickNumber, "Initial computors should use initialTick")
+}
+
 func createTestDataStore(t *testing.T) (*db.PebbleStore, error) {
 	testDir := t.TempDir()
 	err := os.Mkdir(fmt.Sprintf("%s/%s", testDir, "42"), 0755)
@@ -106,7 +230,9 @@ func closeDb(database *db.PebbleStore) {
 }
 
 type TestClient struct {
-	computors types.Computors
+	computors             types.Computors
+	getComputorsCalled    bool
+	getComputorsCallCount int
 }
 
 func (t *TestClient) GetIssuedAssets(_ context.Context, _ string) (types.IssuedAssets, error) {
@@ -154,6 +280,8 @@ func (t *TestClient) GetQuorumVotes(_ context.Context, _ uint32) (types.QuorumVo
 }
 
 func (t *TestClient) GetComputors(_ context.Context) (types.Computors, error) {
+	t.getComputorsCalled = true
+	t.getComputorsCallCount++
 	return t.computors, nil
 }
 
